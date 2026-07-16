2d86e562858a791c071eb238bdbeba0672fec9d7
hasnaintahir1 <hasnaintahir605@gmail.com>
2026-07-16 02:35:43 -0700

    Navbar Update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,11 +2,12 @@ import { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import styles from "./Navbar.module.css";
 
-
-
 const Navbar = () => {
   const [scroll, setScroll] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
+
+  const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -18,14 +19,23 @@ const Navbar = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  const navigate = useNavigate();
-  const location = useLocation();
+  // Page change hote hi menu close
+  useEffect(() => {
+    setMenuOpen(false);
+  }, [location.pathname]);
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
 
   const btnClicked = () => {
+    closeMenu();
     navigate("/");
   };
 
   const goToTechnology = () => {
+    closeMenu();
+
     if (location.pathname !== "/") {
       navigate("/#technology");
     } else {
@@ -36,19 +46,35 @@ const Navbar = () => {
       });
     }
   };
+
   return (
     <nav className={`${styles.nav} ${scroll ? styles.active : ""}`}>
       <h2 onClick={btnClicked}>CineVerse</h2>
 
       <div
-        className={`${styles.links} ${menuOpen ? styles.showMenu : ""
-          }`}
+        className={`${styles.links} ${
+          menuOpen ? styles.showMenu : ""
+        }`}
       >
-        <Link to="/nowplaying">Now Playing</Link>
-        <Link to="/streaming">Streaming</Link>
-        <span onClick={goToTechnology}>Technology</span>
-        <Link to="/about">About Us</Link>
-        <Link to="/advertise">Advertise with us</Link>
+        <Link to="/nowplaying" onClick={closeMenu}>
+          Now Playing
+        </Link>
+
+        <Link to="/streaming" onClick={closeMenu}>
+          Streaming
+        </Link>
+
+        <span onClick={goToTechnology}>
+          Technology
+        </span>
+
+        <Link to="/about" onClick={closeMenu}>
+          About Us
+        </Link>
+
+        <Link to="/advertise" onClick={closeMenu}>
+          Advertise with us
+        </Link>
       </div>
 
       <div
